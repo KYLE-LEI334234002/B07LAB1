5839ee8864239e063d0e671662816e8356b5a003
Kyle Lei <kylelei@Kyles-Mac-mini.local>
2025-09-23 17:11:29 -0400

    change functions to support the new power array system

--- Driver.java
+++ Driver.java
@@ -1,16 +1,24 @@
+import java.util.Arrays;
 public class Driver{
 public static void main(String [] args) {
 Polynomial p = new Polynomial();
 System.out.println(p.evaluate(3));
-double [] c1 = {6,0,0,5};
-Polynomial p1 = new Polynomial(c1);
-double [] c2 = {0,-2,0,0,-9};
-Polynomial p2 = new Polynomial(c2);
+double [] c1 = {6,5};
+int [] c1INT = {0,3};
+Polynomial p1 = new Polynomial(c1, c1INT);
+double [] c2 = {-2,-9};
+int [] c2INT = {1,3};
+Polynomial p2 = new Polynomial(c2, c2INT);
 Polynomial s = p1.add(p2);
-System.out.println("s(0.1) = " + s.evaluate(0.1));
+System.out.println(Arrays.toString(s.array) + "\n" + Arrays.toString(s.powerArray));
+
+System.out.println("s(0.1) = " + s.evaluate(1.0));
+
+/*
 if(s.hasRoot(1))
 System.out.println("1 is a root of s");
 else
 System.out.println("1 is not a root of s");
+*/
 }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,56 +1,89 @@
+import java.util.Arrays;
 
 public class Polynomial {
     double[] array; //set to package private
+    int[] powerArray;
     public Polynomial(){
         array = new double[1];
+        powerArray = new int[1];
         array[0] = 0;
+        powerArray[0] = 0;
     }    
-    public Polynomial(double inputArray[]){
-        array = new double[inputArray.length];
-        for(int i = 0; i < inputArray.length; i++){
-            array[i] = inputArray[i];
-        }
+    public Polynomial(double inputArray[], int inputPowerArray[]){
+        array = Arrays.copyOf(inputArray, inputArray.length);
+        powerArray = Arrays.copyOf(inputPowerArray, inputPowerArray.length);
+        
     }
 
 
     public Polynomial add(Polynomial p2){
-        double[] arrayRev;
-        if(this.array.length < p2.array.length){ //p2 is longer
-            arrayRev = new double[p2.array.length];
-            for(int i = 0; i < p2.array.length; i++){
-                if(i < this.array.length){
-                    arrayRev[i] = this.array[i] + p2.array[i];
+        double[] newArray = new double[0];
+        int[] newPowerArray = new int[0];
+        boolean isMatch;
+        for(int one = 0; one < this.powerArray.length; one++){//go through array one int
+            isMatch = false;
+            for(int two = 0; two < p2.powerArray.length; two++){//go through array two int
+                if(this.powerArray[one] == p2.powerArray[two]){
+                    //
+                    newPowerArray = Arrays.copyOf(newPowerArray, newPowerArray.length + 1);
+                    newPowerArray[newPowerArray.length - 1] = this.powerArray[one];
+                    //add power to the new array
+                    
+                    //
+                    newArray = Arrays.copyOf(newArray, newArray.length + 1);
+                    newArray[newArray.length - 1] = this.array[one] + p2.array[two];
+                    //add coefficient to the new array
+
+                    //
+                    isMatch = true;
+                    // Set the is Match to true so that afterwards things can be added
                 }
-                else{
-                    arrayRev[i] = p2.array[i];
+                //case one if they have the same power
+            }
+            //
+            if(!isMatch){
+                newArray = Arrays.copyOf(newArray, newArray.length + 1);
+                newArray[newArray.length - 1] = this.array[one];
+                //Add Coefficients
+
+                //
+                newPowerArray = Arrays.copyOf(newPowerArray, newPowerArray.length + 1);
+                newPowerArray[newPowerArray.length - 1] = this.powerArray[one];
+                //Insert the (same) power
+
+            }
+            //Add the polynomial entries that weren't a match from this.
+        }
+
+        for(int two = 0; two < p2.powerArray.length; two++){
+            isMatch = false;
+            for(int one = 0; one < this.powerArray.length; one++){
+                if(this.powerArray[one] == p2.powerArray[two]){
+                    isMatch = true;
+                    //making sure we aren't adding the duplicates again
                 }
             }
-        }
-        else if(this.array.length > p2.array.length){// this. is longer
-            arrayRev = new double[this.array.length];
-            for(int i = 0; i < this.array.length; i++){
-                if(i < p2.array.length){
-                    arrayRev[i] = this.array[i] + p2.array[i];
-                }
-                else{
-                    arrayRev[i] = this.array[i];
-                }
+            if(!isMatch){
+                newArray = Arrays.copyOf(newArray, newArray.length + 1);
+                newArray[newArray.length - 1] = p2.array[two];
+                //insert Coefficients, this time with p2
+
+                //
+                newPowerArray = Arrays.copyOf(newPowerArray, newPowerArray.length + 1);
+                newPowerArray[newPowerArray.length - 1] = p2.powerArray[two];
+                //Insert the (same) power, this time with p2
             }
         }
-        else{// equal length
-            arrayRev = new double[this.array.length];
-            for(int i = 0; i < this.array.length; i++){
-                arrayRev[i] = this.array[i] + p2.array[i];
-            }
-        }
-        return new Polynomial(arrayRev);
+        //look through the power array of p2 to add the unique ones
+        return new Polynomial(newArray, newPowerArray);
     }
 
 
     public double evaluate(double value){
         double evalValue = 0.0;
         for(int i = 0; i < array.length; i++){
-            evalValue += array[i] * Math.pow(value, i);
+            evalValue += array[i] * Math.pow(value, powerArray[i]);
+            //this time with powerArray as the power value
         }
         return evalValue;
     }
